2a221ff0e60532d3c6b9a320c00c7e18b84f89d3
xiaokang1206 <chenkang1206@163.com>
2022-06-25 11:00:28 +0800

    first commit

---
+++ git_test/src/main/java/com/athuigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.athuigu.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+
+        System.out.println("hello git !");
+        System.out.println("hello git !");
+        System.out.println("hello git !");
+
+    }
+}
